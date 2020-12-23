6b32666ac26b2a140174d98b2053c45a5859ed89
migger <i.mihailyuk@cian.ru>
2020-12-23 17:36:41 +0300

    removed endpoint

--- index.test.js
+++ index.test.js
@@ -2,19 +2,26 @@ const YandexMQ = require('./index');
 
 describe('main', function () {
     const ymq = new YandexMQ({
-        endpoint: 'https://message-queue.api.cloud.yandex.net/b1glho6pdsm66b2a60a2/dj6000000001g431046d/jwt-public-key',
         region: 'ru-central1',
         keyId: 'oDWgKxttn5gLd3iBIYH7',
         secretKey: '-1uidzTAZaWur-KDEIn_BZh5dpgiUWRBZGZjFl9P',
         verbose: true,
     });
     it('test auth', async () => {
+        let queueName = (process.env.CI_PROJECT_NAME || process.env.USER) + '-' + new Date().getTime();
+        const v1 = await ymq.query('CreateQueue', {QueueName: queueName});
+        const queueUrlOnCreate = v1.CreateQueueResponse.CreateQueueResult[0].QueueUrl[0];
+        const v2 = await ymq.query('GetQueueUrl', {QueueName: queueName});
+        expect(v2.GetQueueUrlResponse.GetQueueUrlResult[0].QueueUrl[0]).toBe(queueUrlOnCreate);
+        
         await ymq.query('SendMessage', {
+            QueueUrl: queueUrlOnCreate,
             MessageBody: "Test",
         });
-        const result = await ymq.query('ReceiveMessage', {});
+        const result = await ymq.query('ReceiveMessage', {QueueUrl: queueUrlOnCreate});
         console.log(JSON.stringify(result));
         expect(result.ReceiveMessageResponse.ReceiveMessageResult[0].Message[0].Body[0]).toBe("Test");
+        await ymq.query('DeleteQueue', {QueueUrl: queueUrlOnCreate});
     });
 
     it('error handling', async () => {
